63b8f2c5a5e3b0e1044c50ff2bcf2ba28a12409a
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-07-15 19:28:33 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+noun = input("noun: ")
+verb = input("verb: ")
+noun2 = input("noun: ")
+noun3 = input("noun: ")
+verb2 = input("verb: ")
+noun4 = input("noun: ")
+
+madlibs = f"hey! Welcome to my {noun}. Here, we {verb} about {noun2} and we have fun with {noun3}. Make sure you {verb2} to my {noun4}."
+
+madlibs2 = f"Every year, you should go visit the doctor. It is a very {adjective11} visit. Usually you have to skip going to {place11} to go. Your doctor is usualy a/n {adjective12} man or wonam who is wearing a/an {adjective13} {pieceofcloth11} "
+
+
+print(madlibs)
