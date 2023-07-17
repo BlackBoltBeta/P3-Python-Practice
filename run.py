087e33a71a02157a9818ed9b33f09647ced1957b
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-07-17 13:39:57 +0000

    add isaplpha to stop non word inputs

--- run.py
+++ run.py
@@ -30,25 +30,25 @@ def madlibs01():
     noun4 = input(colored("noun: ", "blue", attrs=["bold"]))
 
     if noun.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     elif verb.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     elif noun2.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     elif noun3.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     elif verb2.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     elif noun4.isalpha() == False:
-        print_red_bold("Please provide a word")
+        print_red_bold("Please make sure you add only words")
         madlibs01()
     else:
-        madlibs = print_yellow_bold(
+        madlibs1 = print_yellow_bold(
         f"hey! This is my {noun}. It is {verb} for {noun2}" +
         f" and we have fun with {noun3}. " +
         f"Make sure you {verb2} and eat {noun4}."
@@ -59,13 +59,107 @@ def madlibs02():
     """
     Madlibs theme number 2
     """
-    print("this is a filler for madlib number 2")
+    print("NUMBER 2")
+    adjective11 = input(colored("An adjective: ", "blue", attrs=["bold"]))
+    adjective12 = input(colored("An adjective: ", "blue", attrs=["bold"]))
+    adjective13 = input(colored("Another adjective: ", "blue", attrs=["bold"]))
+    adjective14 = input(colored("Adjective please: ", "blue", attrs=["bold"]))
+    adjective15 = input(colored("Adjective again: ", "blue", attrs=["bold"]))
+    place11 = input(colored("A place: ", "blue", attrs=["bold"]))
+    pieceofcloth11 = input(colored("A piece of clothing: ", "blue", attrs=["bold"]))
+    bodypart11 = input(colored("a body part: ", "blue", attrs=["bold"]))
+    bodypart12 = input(colored("another body part: ", "blue", attrs=["bold"]))
+    bodypart13 = input(colored("a third body part: ", "blue", attrs=["bold"]))
+    noun11 = input(colored("a noun: ", "blue", attrs=["bold"]))
+    noun12 = input(colored("a second noun: ", "blue", attrs=["bold"]))
+    place12 = input(colored("another place: ", "blue", attrs=["bold"]))
+
+    if adjective11.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif adjective12.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif adjective13.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif adjective14.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif adjective15.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif place11.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif pieceofcloth11.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif bodypart11.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif bodypart12.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif bodypart13.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif noun11.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif noun12.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    elif place12.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs02()
+    else:
+        madlibs2 = print_yellow_bold(
+            f"Every year, you visit the doctor. It is very {adjective11}. " +
+            f"Usually you have to skip going to {place11} to go. Your doctor " +
+            f"is usualy a {adjective12} person who is wearing a/an " +
+            f"{adjective13} {pieceofcloth11}. They will look at your {bodypart11}," +
+            f" {bodypart12} and {bodypart13}. They can be very" +
+            f" {adjective14}. Afterwards, they will give you a {noun11} and a " +
+            f"{noun12}, and then you will go to {place12} as a treat." +
+            f" All in all, the doctor's office isn't so {adjective15}."
+        )
 
 def madlibs03():
     """
     Madlibs theme number 3
     """
-    print("this is a filler for madlib number 3")
+    noun = input(colored("noun: ", "blue", attrs=["bold"]))
+    verb = input(colored("verb: ", "blue", attrs=["bold"]))
+    noun2 = input(colored("noun: ", "blue", attrs=["bold"]))
+    noun3 = input(colored("noun: ", "blue", attrs=["bold"]))
+    verb2 = input(colored("verb: ", "blue", attrs=["bold"]))
+    noun4 = input(colored("noun: ", "blue", attrs=["bold"]))
+
+    if noun.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    elif verb.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    elif noun2.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    elif noun3.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    elif verb2.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    elif noun4.isalpha() == False:
+        print_red_bold("Please make sure you add only words")
+        madlibs03()
+    else:
+        madlibs3 = print_yellow_bold(
+        f"hey! This is my {noun}. It is {verb} for {noun2}" +
+        f" and we have fun with {noun3}. " +
+        f"Make sure you {verb2} and eat {noun4}."
+    )
 
 
 def main_choice():
@@ -106,17 +200,13 @@ adjective12 = input("adjective two: ")
 adjective13 = input("adjective three: ")
 adjective14 = input("adjective four: ")
 adjective15 = input("adjective five: ")
-
 place11 = input("a place: ")
 pieceofcloth11 = input("piece of clothing: ")
-
 bodypart11 = input("a body part: ")
 bodypart12 = input("another body part: ")
 bodypart13 = input("a third body part: ")
-
 noun11 = input("a noun: ")
 noun12 = input("a second noun: ")
-
 place12 = input("another place: ")
 
 madlibs2 = f"Every year, you should go visit the doctor. It is a very {adjective11} visit. Usually you have to skip going to {place11} to go. Your doctor is usualy a/n {adjective12} man or wonam who is wearing a/an {adjective13} {pieceofcloth11}. They will look at your {bodypart11}, {bodypart12} and {bodypart13}. Sometimes, they can be very {adjective14}. Afterwards, they will give you a {noun11} and a {noun12}, and your mom or dad will take you to {place12} as a treat. All in all, the doctor's office isn't so {adjective15}."
